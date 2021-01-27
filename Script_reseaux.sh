011cd5510b27262e999eae0ab24989b714c716b5
SanchezYoan <77174458+SanchezYoan@users.noreply.github.com>
2021-01-27 21:12:50 +0100

    Add files via upload

--- Script_reseaux.sh
+++ Script_reseaux.sh
@@ -4,8 +4,18 @@ sudo apt update
 sudo apt install mysql-server
 sudo mysql_secure_installation
 
-conn = sqlite3.connect(args.db_path)
-c = conn.cursor()
+sudo mysql
+SET PASSWORD FOR root@localhost=PASSWORD('azerty');
+GRANT ALL PRIVILEGES ON *.* TO root@localhost IDENTIFIED BY 'xxxxxx' WITH GRANT OPTION;
+
+CREATE DATABASE gestion_de_la_facturation;
+import mySQLite3
+conn = mysql.connector.connect(host="192.168.60.18",
+                               user="shadowleader", password="what-else?", 
+                               database="magasin")
+cursor = conn.cursor()
+# Opérations à réaliser sur la base ...
+conn.close()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS "gestion de la facturation " (
     "Factures"	TEXT,
@@ -35,7 +45,7 @@ echo "Installing bases packages"
 sudo apt-get install --yes git-extras python3-pip
 
 
-~/.ssh/authorized_keys du serveur
+/.ssh/authorized_keys du serveur
 	
 # Le port SSH par défaut est 22
 ssh-copy-id -i ~/.ssh/informatix.pub "<utilisateur>@<hote> -p <port>"
