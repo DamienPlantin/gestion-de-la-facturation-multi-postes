afabbf3aa4b76d6b9136be7c152843c85f9205b5
SanchezYoan <77174458+SanchezYoan@users.noreply.github.com>
2021-02-02 23:52:16 +0100

    Add files via upload

--- date.py
+++ date.py
@@ -3,6 +3,7 @@ import sys
 import mysql.connector
 
 
+
 # logger = logging.getLogger('Date')
 # logger.setLevel(logging.DEBUG)
 
@@ -21,12 +22,13 @@ parser = argparse.ArgumentParser("Script to interact with data from the Facturat
 parser.add_argument("-au", "--ajouter_user", help="ajouter un utilisateur à mysql")
 parser.add_argument("-ac", "--ajouter_client", nargs="*", help="Ajouter un client avec les infos nécéssaires")
 parser.add_argument("-af", "--ajouter_facture", nargs="*", help="Ajouter une facture à la base de donnee")
+# parser.add_argument("-al", "--ajouter_ligne", nargs="*", "ajouter la facture du produit detaille")
 parser.add_argument("-m", "--modifier_client", help="Modifier les infos du client")
 parser.add_argument("-r", "--rechercher_client", nargs="*", help="Rechercher un client avec son nom ou son e-mail")
 parser.add_argument("-f", "--rechercher_facture", help="rechercher une facture avec son numero")
 parser.add_argument("-fs", "--factures", nargs="*", help="afficher toutes les factures entre deux dates")
-parser.add_argument("-s", "--suprimmer", nargs="*", help="Suprimmer des informations de la base de donnée")
-parser.add_argument("-t", "--total", help="Afficher le total des factures par défauts")
+parser.add_argument("-s", "--supprimer", nargs="*", help="Suprimmer des informations de la base de donnée")
+# parser.add_argument("-r", "--remise", help="Modifier le montant total avec une remise")
 args = parser.parse_args()
 
 # conn = mysql.connector.connect(host="localhost", user = "root", password = "",database = "GESTION")
@@ -35,43 +37,44 @@ args = parser.parse_args()
 
 addu = args.ajouter_user
 addc = args.ajouter_client
+addl = args.ajouter_ligne
 addf = args.ajouter_facture
 modc = args.modifier_client
 rechc = args.rechercher_client 
 rechf = args.rechercher_facture 
-affs = args.factures
-sup = args.suprimmer
+betf = args.factures
+supp = args.suprimmer
+remi = args.remise
+# sup = args.suprimmer
 
-conn = mysql.connector.connect(host='192.168.168.19', database='GESTION', user='yoyo34', password='AzErTy123*')
+conn = mysql.connector.connect(host='192.168.242.195', database='GESTION', user='employer', password='AzErTy123*')
 cursor = conn.cursor()
-# try:
-#     conn = mysql.connector.connect(host='3306', database='GESTION', user='root', password='Ardkordu30')
-#     cursor = conn.cursor()
-
-# except mysql.connector.Error as error:
-#     print("Les tables n'ont pas été créer dans MySQL: {}".format(error))
 
 def add_user():             
     #Créé un utilisateur 
-    cursor.execute("""CREATE USER '?' IDENTIFIED WITH auth_socket""", (addu))
+    cursor.execute("""CREATE USER '?' IDENTIFIED WITH mysql_native_password""", (addu))
 # def user_connect():
 #     cursor.execute('SELECT user, host, plugin, authentication_string FROM mysql.user)
 
 def add_client():
     #Ajouter les informations d'un client
-    
+
     cursor.execute('INSERT INTO gestion_client (TYPE_DE_CLIENT, RAISON_SOCIALE, EMAIL, TELEPHONE, ADRESSE ) VALUES (?,?,?,?,?)', (addc[0],addc[1], addc[2], addc[3], addc[4]))
 
-def add_facture():
-    #Ajouter une facture à la bdd
+def add_ligne():
+    #Ajouter une ligne facture à la bdd
 
-    cursor.execute('INSERT INTO gestion_facture (NOM_DU_PRODUIT, REFERENCE, QUANTITE, PRIX_UNITE, N_FACTURE, CLIENT,DATE_EMISSION,MONTANT_TOTAL ) VALUES (?,?,?,?,?,?,?,?)', (addf[0],addf[1],addf[2],addf[3], addf[4], addf[5],addf[6],addf[7]))
-   
+    cursor.execute('INSERT INTO gestion_client (N_FACTURE, CLIENT,DATE_EMISSION) VALUES (?,?,?)', (addl[0], addl[1], addl[2]))
+
+def add_facture():
+    #Ajouter une facture détaille à la bdd
+
+    cursor.execute('INSERT INTO gestion_facture (NOM_DU_PRODUIT, REFERENCE, QUANTITE, PRIX_UNITE) VALUES (?,?,?,?)', (addf[0],addf[1],addf[2],addf[3]))
 
 def rech_factures():
     # rechercher une facture avec le numéro dans la bdd
 
-    cursor.execute("""SELECT N_FACTURE FROM gestion_facture WHERE '?' """, (rechf))
+    cursor.execute("""SELECT N_FACTURE FROM gestion_ligne WHERE '?' """, (rechf))
 
 def rech_client(): 
     #rechercher un client avec son nom et/ou son mail
@@ -80,17 +83,33 @@ def rech_client():
 
 def modif_client():
     #Modifier les infos d'un client
+
     cursor.execute("""UDPDATE gestion_client SET '?' = '?' WHERE '?' """, (modc[0], modc[1], modc[3]))
 
-def date():
+def between_date():
     #Ajouter une date à la facturation
-    
-    cursor.execute("""SELECT * FROM gestion_facture WHERE DATE_EMISSION BETWEEN '?' AND '?' """, (affs[0],affs[1]))
+
+    cursor.execute("""SELECT * FROM gestion_facture WHERE DATE_EMISSION BETWEEN '?' AND '?' """, (betf[0],betf[1]))
     for i in cursor.fetchall():
         factures = (f'NOM_DU_PRODUIT{i[0]} REFERENCE{i[1]} QUANTITE{i[2]} PRIX_UNITE{[3]} N_FACTURE{i[4]} CLIENT{i[5]} DATE_EMISSION{i[6]} MONTANT_TOTAL{i[7]}')
         print(factures)
 
+# def montant_total():
+#     Total = []
+#     if addl:
+#         result = QUANTITE*PRIX_UNITE
+#         Total.append(result)
 
+#     cursor.execute("""  """)
+
+def remise():
+    #Ajouter une remise pour la déduire sur le prix
+    remise = (remi)*100/result
+
+def supprimer():
+    #Supprimer une ligne dans la bdd
+    cursor.execute("""DELETE FROM `'?' `WHERE '?'""", (supr[0], supr[1]))
+    
 
 def main():
     #Création de la db, des tables de gestion client et facture
@@ -105,17 +124,21 @@ def main():
     "ADRESSE" INTEGER
     );""")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS "gestion_facture" (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS "gestion_ligne" (
     "NOM_DU_PRODUIT" TEXT,
     "REFERENCE" INTEGER,
     "QUANTITE" INTEGER,
     "PRIX_UNITE" INTEGER,
+    );""")
 
+    cursor.execute("""CREATE TABLE IF NOT EXISTS "gestion_facture"(
     "N_FACTURE" TEXT,
     "CLIENT" TEXT,
     "DATE_EMISSION"	DATE,
     "MONTANT_TOTAL" INTEGER
     );""")
+
+
 
 
     if addc:
